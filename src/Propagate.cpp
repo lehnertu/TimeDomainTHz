4fc2c88fa139ce0d04f122823498c3a48be7479d
Ulf Lehnert <ulf@lehnertu.de>
2020-03-12 10:42:33 +0100

    first complete version of the derivatives needed for propagation

--- src/Propagate.cpp
+++ src/Propagate.cpp
@@ -78,6 +78,9 @@ int main(int argc, char* argv[])
         avg_xi += source->xi[ip];
         avg_eta += source->eta[ip];
     };
+    avg_normal /= source->get_Np();
+    avg_xi /= source->get_Np();
+    avg_eta /= source->get_Np();
     std::cout << "n =   (" << avg_normal.x << ", " << avg_normal.y << ", " << avg_normal.z << ")" << std::endl;
     std::cout << "xi =  (" << avg_xi.x << ", " << avg_xi.y << ", " << avg_xi.z << ")" << std::endl;
     std::cout << "eta = (" << avg_eta.x << ", " << avg_eta.y << ", " << avg_eta.z << ")" << std::endl;
@@ -86,57 +89,169 @@ int main(int argc, char* argv[])
     // test the computation of derivatives
     // **************************************
     // temporarily we define most variables of the screen as public
+    timespec start_time, stop_time;
 
-    std::cout << "computing the derivatives of the fields ..." << std::endl;
+    std::cout << "computing the time derivatives of the fields ..." << std::endl;
     // record the start time
-    timespec start_time;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
-    
-    int index = 1190;
-    int nbh[20];
-    int count = source->get_Neighbourhood(index,nbh);
-    std::cout << "neighbourhood size = " << count << std::endl;
-    std::cout << "[";
-    for (int i=0; i<count; i++) std::cout << nbh[i] << " ";
-    std::cout << "]" << std::endl;
-    
-    // an array of 20 pointers to field traces in the local coordinate system
-    FieldTrace* local_trace[20];
-    Eigen::VectorXd Ex(count);
-    Eigen::VectorXd xi(count), eta(count), xi2(count), eta2(count), xieta(count), one(count);
-    for (int i=0; i<count; i++)
-    {
-        local_trace[i] = new FieldTrace(source->A[nbh[i]]);
-        Ex(i) = local_trace[i]->get_field(200).E().x;
-        Vector local_coo = source->get_point(nbh[i]) - source->get_point(index);
-        local_coo.transform(source->xi[index],source->eta[index],source->normal[index]);
-        one(i) = 1.0;
-        xi(i) = local_coo.x;
-        eta(i) = local_coo.y;
-        xi2(i) = local_coo.x * local_coo.x;
-        eta2(i) = local_coo.y * local_coo.y;
-        xieta(i) = local_coo.x * local_coo.y;
-        local_trace[i]->transform(source->xi[index],source->eta[index],source->normal[index]);
-    };
-    std::cout << "Ex = [" << Ex << "]" << std::endl;
-    // we solve anequation A x = Ex in a least-squares sense
-    Eigen::MatrixXd A(count,6);
-    A.col(0) << one;
-    A.col(1) << xi;
-    A.col(2) << eta;
-    A.col(3) << xi2;
-    A.col(4) << eta2;
-    A.col(5) << xieta;
-    cout << "Here is the matrix A:\n" << A << endl;
-    cout << "Here is the right hand side Ex:\n" << Ex << endl;
-    cout << "The least-squares solution is:\n"
-        << A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(Ex) << endl;
-    for (int i=0; i<count; i++) delete local_trace[i];
 
+    // compute a full set of time derivatives of the fields
+    std::vector<FieldTrace*> source_dA_dt = std::vector<FieldTrace*>(source->get_Np());
+    for (int ip=0; ip<source->get_Np(); ip++)
+    {
+        FieldTrace source_trace = source->A[ip];
+        FieldTrace *deriv = new FieldTrace(source_trace.derivative());
+        source_dA_dt[ip] = deriv;
+    }
+    
     // record the finish time
-    timespec stop_time;
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop_time);
     double elapsed = stop_time.tv_sec-start_time.tv_sec +
+        1e-9*(stop_time.tv_nsec-start_time.tv_nsec);
+    std::cout << "time elapsed during computation : " << elapsed << " s" << std::endl;
+
+    // prepare a full set of normal derivatives of the fields
+    // these will be filled with data once we have computed the transverse derivatives
+    std::vector<FieldTrace*> source_dA_dn = std::vector<FieldTrace*>(source->get_Np());
+    for (int ip=0; ip<source->get_Np(); ip++)
+    {
+        FieldTrace *deriv = new FieldTrace(source->A[ip]);
+        source_dA_dn[ip] = deriv;
+    }
+
+    std::cout << "computing the spatial derivatives of the fields ..." << std::endl;
+    // record the start time
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start_time);
+    
+    double cSquared = SpeedOfLight*SpeedOfLight;
+    int NOTS = source->get_Nt();
+    
+    // loop over the index of the cell considered
+    for (int index=0; index<source->get_Np(); index++)
+    {    
+        // determine the neighbourhood of this cell
+        int nbh[24];
+        int count = source->get_Neighbourhood(index,nbh);
+        // std::cout << "neighbourhood size = " << count << std::endl;
+        // std::cout << "[";
+        // for (int i=0; i<count; i++) std::cout << nbh[i] << " ";
+        // std::cout << "]" << std::endl;
+        
+        // get the coordinates of the neighbours in the cell-local coordinate system
+        // assemble these into a matrix A
+        Vector local_ref = source->get_point(index);
+        Vector local_xi = source->xi[index];
+        Vector local_eta = source->eta[index];
+        Vector local_n = source->normal[index];
+        Eigen::VectorXd xi(count), eta(count), xi2(count), eta2(count), xieta(count), one(count);
+        for (int i=0; i<count; i++)
+        {
+            Vector local_coo = source->get_point(nbh[i]) - local_ref;
+            local_coo.transform(local_xi,local_eta,local_n);
+            one(i) = 1.0;
+            xi(i) = local_coo.x;
+            eta(i) = local_coo.y;
+            xi2(i) = local_coo.x * local_coo.x;
+            eta2(i) = local_coo.y * local_coo.y;
+            xieta(i) = local_coo.x * local_coo.y;
+        };
+        Eigen::MatrixXd A(count,6);
+        A.col(0) << one;
+        A.col(1) << xi;
+        A.col(2) << eta;
+        A.col(3) << xi2;
+        A.col(4) << eta2;
+        A.col(5) << xieta;
+        
+        // get the field traces of the neighbourhood in the local coordinate system
+        FieldTrace* local_trace = new FieldTrace(source->A[index]);
+        FieldTrace* nbh_trace[24];
+        for (int i=0; i<count; i++)
+        {
+            nbh_trace[i] = new FieldTrace(source->A[nbh[i]]);
+            nbh_trace[i]->transform(local_xi,local_eta,local_n);
+        };
+        
+        // get the time derivative of the center field in local coordinates
+        FieldTrace local_dA_dt = source_dA_dt[index];
+        local_dA_dt.transform(local_xi,local_eta,local_n);
+        
+        // this is the trace of normal derivatives we have to compute
+        FieldTrace *local_dA_dn = source_dA_dn[index];
+        
+        for (int it=0; it<NOTS; it++)
+        {
+            // we solve the equation A x = E, A x = B in a least-squares sense
+            Eigen::VectorXd Exi(count);
+            Eigen::VectorXd Eeta(count);
+            Eigen::VectorXd En(count);
+            Eigen::VectorXd Bxi(count);
+            Eigen::VectorXd Beta(count);
+            Eigen::VectorXd Bn(count);
+            Eigen::VectorXd fit(6);
+            double t = local_trace->get_time(it);
+            for (int i=0; i<count; i++)
+            {
+                // we do not query the fields for the same time index but for the same time
+                ElMagField F = nbh_trace[i]->get_field(t);
+                Exi(i) = F.E().x;
+                Eeta(i) = F.E().y;
+                En(i) = F.E().z;
+                Bxi(i) = F.B().x;
+                Beta(i) = F.B().y;
+                Bn(i) = F.B().z;
+            };
+            // the solution delivers the spatial derivatives of the fields
+            fit = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(Exi);
+            double dx_Ex = fit(1);
+            // double dy_Ex = fit(2);
+            fit = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(Eeta);
+            // double dx_Ey = fit(1);
+            double dy_Ey = fit(2);
+            fit = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(En);
+            double dx_Ez = fit(1);
+            double dy_Ez = fit(2);
+            fit = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(Bxi);
+            double dx_Bx = fit(1);
+            // double dy_Bx = fit(2);
+            fit = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(Beta);
+            // double dx_By = fit(1);
+            double dy_By = fit(2);
+            fit = A.bdcSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(Bn);
+            double dx_Bz = fit(1);
+            double dy_Bz = fit(2);
+
+            // obtain the time-derivatives of the fiels
+            ElMagField dt_F = local_dA_dt.get_field(it);
+            double dt_Ex = dt_F.E().x;
+            double dt_Ey = dt_F.E().y;
+            double dt_Bx = dt_F.B().x;
+            double dt_By = dt_F.B().y;
+            
+            // use Maxwells equations to compute the normal derivatives of the fields
+            double dz_Ex = dx_Ez - dt_By;
+            double dz_Ey = dy_Ez + dt_Bx;
+            double dz_Ez = -dx_Ex - dy_Ey;
+            double dz_Bx = dx_Bz + dt_Ey/cSquared;
+            double dz_By = dy_Bz - dt_Ex/cSquared;
+            double dz_Bz = -dx_Bx - dy_By;
+
+            // we set the normal derivatives by back-transforming the computed values
+            // into the global coordinate system
+            Vector E = local_xi*dz_Ex + local_eta*dz_Ey + local_n*dz_Ez;
+            Vector B = local_xi*dz_Bx + local_eta*dz_By + local_n*dz_Bz;
+            ElMagField f = ElMagField(E,B);
+            local_dA_dn->set(it,f);
+        };
+
+        // clean up the traces
+        for (int i=0; i<count; i++) delete nbh_trace[i];
+        delete local_trace;
+    };
+    
+    // record the finish time
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &stop_time);
+    elapsed = stop_time.tv_sec-start_time.tv_sec +
         1e-9*(stop_time.tv_nsec-start_time.tv_nsec);
     std::cout << "time elapsed during computation : " << elapsed << " s" << std::endl;
     
@@ -159,6 +274,9 @@ int main(int argc, char* argv[])
         avg_xi += target->xi[ip];
         avg_eta += target->eta[ip];
     };
+    avg_normal /= target->get_Np();
+    avg_xi /= target->get_Np();
+    avg_eta /= target->get_Np();
     std::cout << "n =   (" << avg_normal.x << ", " << avg_normal.y << ", " << avg_normal.z << ")" << std::endl;
     std::cout << "xi =  (" << avg_xi.x << ", " << avg_xi.y << ", " << avg_xi.z << ")" << std::endl;
     std::cout << "eta = (" << avg_eta.x << ", " << avg_eta.y << ", " << avg_eta.z << ")" << std::endl;
@@ -172,7 +290,9 @@ int main(int argc, char* argv[])
         if (t0<min_t0) min_t0=t0;
     }
     std::cout << "target start timing range (" << min_t0 << ", " << max_t0 << ")" << std::endl;
-    
+
+    for (int ip=0; ip<source->get_Np(); ip++) delete source_dA_dt[ip];
+    for (int ip=0; ip<source->get_Np(); ip++) delete source_dA_dn[ip];
     delete source;
     delete target;
     
